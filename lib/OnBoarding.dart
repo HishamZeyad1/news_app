f3cf776a74d54537517bce29ac4a87f8f24737ba
Hisham <hishamzeyad98@gmail.com>
2021-02-08 11:48:07 +1200

    add page view indicators

--- lib/OnBoarding.dart
+++ lib/OnBoarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'PageModel.dart';
+import 'package:page_view_indicator/page_view_indicator.dart';
+
 class OnBoarding extends StatefulWidget {
   @override
   _OnBoardingState createState() => _OnBoardingState();
@@ -7,8 +9,9 @@ class OnBoarding extends StatefulWidget {
 
 class _OnBoardingState extends State<OnBoarding> {
   List<PageModel> pages;
-// ValueNotifier<int> _pageViewNotifier = ValueNotifier(0);
-  void _addPages(){
+  ValueNotifier<int> _pageViewNotifier = ValueNotifier(0);
+
+  void _addPages() {
     pages = List<PageModel>();
     pages.add(PageModel('Welcome',
         '1- Making friends is easy as waving your hand back and forth in easy step',
@@ -27,85 +30,122 @@ class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
     _addPages();
-    return  Stack(
-        children: <Widget>[
-          Scaffold(
-            body: PageView.builder(
-              // ignore: missing_return
-              itemBuilder: (context, index) {
-                return Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                        image: ExactAssetImage(pages[ index ].image),
-                        fit: BoxFit.cover,
-                      )),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Transform.translate(
-                          child: Icon(
-                            pages[ index ].icon,
-                            size: 100,
-                            color: Colors.white,
-                          ),
-                          offset: Offset(0, -50),
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          body: PageView.builder(
+            // ignore: missing_return
+            itemBuilder: (context, index) {
+              return Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: ExactAssetImage(pages[ index ].image),
+                          fit: BoxFit.cover,
+                        )),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Transform.translate(
+                        child: Icon(
+                          pages[ index ].icon,
+                          size: 100,
+                          color: Colors.white,
                         ),
-                        Text(
-                          pages[index].title,
+                        offset: Offset(0, -50),
+                      ),
+                      Text(
+                        pages[index].title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 48, right: 48, top: 18),
+                        child: Text(
+                          pages[ index ].description,
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                            fontSize: 16,
                           ),
                           textAlign: TextAlign.center,
-
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 48, right: 48, top: 18),
-                          child: Text(
-                            pages[ index ].description,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                              ),
-                              textAlign: TextAlign.center,
-                              ),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              },
-              itemCount: pages.length,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 24),
-              child: SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: RaisedButton(
-                  color: Colors.red.shade900,
-                  child: Text(
-                    'GET STARTED',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      letterSpacing: 1,
-                    ),
+                      ),
+                    ],
                   ),
-                  onPressed: () {},
+                ],
+              );
+            },
+            itemCount: pages.length,
+            onPageChanged: (index) {
+              _pageViewNotifier.value = index;
+            },
+
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(0, 175),
+          child: Align(
+            alignment: Alignment.center,
+            child: _displayPageIndicators(pages.length),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 24),
+            child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: RaisedButton(
+                color: Colors.red.shade900,
+                child: Text(
+                  'GET STARTED',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    letterSpacing: 1,
+                  ),
                 ),
+                onPressed: () {},
               ),
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
+  }
+
+  Widget _displayPageIndicators(int length) {
+    return PageViewIndicator(
+      pageIndexNotifier: _pageViewNotifier,
+      length: length,
+      normalBuilder: (animationController, index) =>
+          Circle(
+            size: 8.0,
+            color: Colors.grey,
+          ),
+      highlightedBuilder: (animationController, index) =>
+          ScaleTransition(
+            scale: CurvedAnimation(
+              parent: animationController,
+              curve: Curves.ease,
+            ),
+            child: Circle(
+              size: 12.0,
+              color: Colors.red,
+            ),
+          ),
+    );
   }
 }
+
+
